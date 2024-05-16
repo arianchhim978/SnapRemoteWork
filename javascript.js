40a21980a4ef975089e2cf99eb5a3773250283c0
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2024-05-16 12:45:03 -0400

    saved files

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById("modal")
+
+
+
+
+function a (){
+
+    if(modal.style.class === "block"){
+        modal.style.class = 'none'
+    } else {
+        modal.style.class='block'
+    }
+
+
+}
